509fb9f079c08dd22107b8e6245c271774eb2ff8
Sebastian <sebapazoss@MacBook-Air-de-Sebastian.local>
2026-06-28 20:01:35 -0300

    corregimos url

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ const isProduction = window.location.hostname !== 'localhost' && window.location
 
 // API Base URL
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || (isProduction
-  ? 'https://e-commerse-pazos-vedoya.vercel.app/api/api'  // Cambiar en producción o usar variable de entorno
+  ? 'https://e-commerse-pazos-vedoya.vercel.app/api'  // Cambiar en producción o usar variable de entorno
   : 'http://localhost:8000/api');
 
 const api = axios.create({
